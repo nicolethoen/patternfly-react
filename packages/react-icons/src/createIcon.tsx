a81ce0e1b6fdd7a3e85cdd70e5bb667dc7d82801
kmcfaul <45077788+kmcfaul@users.noreply.github.com>
2026-02-18 16:55:13 -0500

    feat(icons): add swap support for mapped rh-ui icons (#12245)
    * feat(icons): investigating pf-rh icon hotswapping
    
    * update createIcon tests for new structure
    
    * change to css svg logic
    
    * upddate snapshots & logic for rhuiicon check
    
    * feat(icon): pull in core bump and clean up PR
    
    * reupdate snap from spike, move icon mapping to icons folder
    
    * remove unneeded checks from else block, fix tsconfig
    
    * update static rendering script with new createIcon format
    
    * add warning for set being specified without rhui icon data

--- packages/react-icons/src/createIcon.tsx
+++ packages/react-icons/src/createIcon.tsx
@@ -4,76 +4,147 @@ export interface SVGPathObject {
   path: string;
   className?: string;
 }
+
 export interface IconDefinition {
   name?: string;
   width: number;
   height: number;
-  svgPath: string | SVGPathObject[];
+  svgPathData: string | SVGPathObject[];
   xOffset?: number;
   yOffset?: number;
   svgClassName?: string;
 }
 
+export interface CreateIconProps {
+  name?: string;
+  icon?: IconDefinition;
+  rhUiIcon?: IconDefinition | null;
+}
+
 export interface SVGIconProps extends Omit<React.HTMLProps<SVGElement>, 'ref'> {
   title?: string;
   className?: string;
+  /* Indicates the icon should render using alternate svg data for unified theme */
+  set?: 'default' | 'rh-ui';
 }
 
 let currentId = 0;
 
+const createSvg = (icon: IconDefinition, iconClassName: string) => {
+  const { xOffset, yOffset, width, height, svgPathData, svgClassName } = icon ?? {};
+  const _xOffset = xOffset ?? 0;
+  const _yOffset = yOffset ?? 0;
+  const viewBox = [_xOffset, _yOffset, width, height].join(' ');
+
+  const classNames = [];
+
+  if (svgClassName) {
+    classNames.push(svgClassName);
+  }
+  if (iconClassName) {
+    classNames.push(iconClassName);
+  }
+
+  const svgPaths =
+    svgPathData && Array.isArray(svgPathData) ? (
+      svgPathData.map((pathObject, index) => (
+        <path className={pathObject.className} key={`${pathObject.path}-${index}`} d={pathObject.path} />
+      ))
+    ) : (
+      <path d={svgPathData as string} />
+    );
+
+  return (
+    <svg viewBox={viewBox} className={classNames.join(' ')}>
+      {svgPaths}
+    </svg>
+  );
+};
+
 /**
  * Factory to create Icon class components for consumers
  */
-export function createIcon({
-  name,
-  xOffset = 0,
-  yOffset = 0,
-  width,
-  height,
-  svgPath,
-  svgClassName
-}: IconDefinition): React.ComponentClass<SVGIconProps> {
+export function createIcon({ name, icon, rhUiIcon = null }: CreateIconProps): React.ComponentClass<SVGIconProps> {
   return class SVGIcon extends Component<SVGIconProps> {
     static displayName = name;
 
     id = `icon-title-${currentId++}`;
 
+    constructor(props: SVGIconProps) {
+      super(props);
+    }
+
     render() {
-      const { title, className: propsClassName, ...props } = this.props;
+      const { title, className: propsClassName, set, ...props } = this.props;
 
       const hasTitle = Boolean(title);
-      const viewBox = [xOffset, yOffset, width, height].join(' ');
-
       const classNames = ['pf-v6-svg'];
-      if (svgClassName) {
-        classNames.push(svgClassName);
-      }
+
       if (propsClassName) {
         classNames.push(propsClassName);
       }
 
-      return (
-        <svg
-          className={classNames.join(' ')}
-          viewBox={viewBox}
-          fill="currentColor"
-          aria-labelledby={hasTitle ? this.id : null}
-          aria-hidden={hasTitle ? null : true}
-          role="img"
-          width="1em"
-          height="1em"
-          {...(props as Omit<React.SVGProps<SVGElement>, 'ref'>)} // Lie.
-        >
-          {hasTitle && <title id={this.id}>{title}</title>}
-          {Array.isArray(svgPath) ? (
-            svgPath.map((pathObject, index) => (
+      if (set === 'rh-ui' && rhUiIcon === null) {
+        // eslint-disable-next-line no-console
+        console.warn(
+          `Set "rh-ui" was provided for ${name}, but no rh-ui icon data exists for this icon. The default icon will be rendered.`
+        );
+      }
+
+      if ((set === undefined && rhUiIcon === null) || set !== undefined) {
+        const iconData = set !== undefined && set === 'rh-ui' && rhUiIcon !== null ? rhUiIcon : icon;
+        const { xOffset, yOffset, width, height, svgPathData, svgClassName } = iconData ?? {};
+        const _xOffset = xOffset ?? 0;
+        const _yOffset = yOffset ?? 0;
+        const viewBox = [_xOffset, _yOffset, width, height].join(' ');
+
+        if (svgClassName) {
+          classNames.push(svgClassName);
+        }
+
+        const svgPaths =
+          svgPathData && Array.isArray(svgPathData) ? (
+            svgPathData.map((pathObject, index) => (
               <path className={pathObject.className} key={`${pathObject.path}-${index}`} d={pathObject.path} />
             ))
           ) : (
-            <path d={svgPath} />
-          )}
-        </svg>
-      );
+            <path d={svgPathData as string} />
+          );
+
+        return (
+          <svg
+            className={classNames.join(' ')}
+            fill="currentColor"
+            viewBox={viewBox}
+            aria-labelledby={hasTitle ? this.id : null}
+            aria-hidden={hasTitle ? null : true}
+            role="img"
+            width="1em"
+            height="1em"
+            {...(props as Omit<React.SVGProps<SVGElement>, 'ref'>)} // Lie.
+          >
+            {hasTitle && <title id={this.id}>{title}</title>}
+            {svgPaths}
+          </svg>
+        );
+      } else {
+        return (
+          <svg
+            className={classNames.join(' ')}
+            fill="currentColor"
+            aria-labelledby={hasTitle ? this.id : null}
+            aria-hidden={hasTitle ? null : true}
+            role="img"
+            width="1em"
+            height="1em"
+            {...(props as Omit<React.SVGProps<SVGElement>, 'ref'>)} // Lie.
+          >
+            {hasTitle && <title id={this.id}>{title}</title>}
+            {icon && createSvg(icon, 'pf-v6-icon-default')}
+            {rhUiIcon && createSvg(rhUiIcon, 'pf-v6-icon-rh-ui')}
+          </svg>
+        );
+      }
     }
   };
 }
